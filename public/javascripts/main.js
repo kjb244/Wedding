49e849696f729f9bfcb9b054a81f1eff55564ebc
kjb244 <kevin.bacino@gmail.com>
2018-02-03 09:27:48 -0500

    changed to use router instead of switch

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,47 @@
-var app = angular.module("myApp", ["ngAnimate"]);
+var app = angular.module("myApp", ["ngRoute","ngAnimate"]);
 
+app.config(function($routeProvider) {
+    $routeProvider
 
+    .when('/', {
+        templateUrl: 'route_templates/route-home.html',
+        controller: 'ctrlMain'
+    })
+    .when('/home', {
+        templateUrl: 'route_templates/route-home.html',
+        controller: 'ctrlMain'
+    })
+    .when('/story', {
+        templateUrl: 'route_templates/route-story.html',
+        controller: 'ctrlMain'
+    })
+    .when('/schedule', {
+        templateUrl: 'route_templates/route-schedule.html',
+        controller: 'ctrlMain'
+    })
+    .when('/things-to-do', {
+        templateUrl: 'route_templates/route-things-to-do.html',
+        controller: 'ctrlMain'
+    })
+    .when('/rsvp', {
+        templateUrl: 'route_templates/route-rsvp.html',
+        controller: 'ctrlMain'
+    });
+});
 
 
 
 app.controller("ctrlMain", function($scope,ajaxFetch ) {
     $scope.menuChange = 'Home';
-    ajaxFetch.getData('/getContent')
-        .then(function(res) {
-            $scope.copy = res.data;
-        });
+    if(!document.querySelector('body').classList.contains('content-loaded')) {
+        ajaxFetch.getData('/getContent')
+            .then(function (res) {
+                $scope.copy = res.data;
+                document.querySelector('body').classList.add('content-loaded');
+            });
+    }
+
+
 
 });
 

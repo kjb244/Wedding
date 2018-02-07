0bc4629e1870b308cba1a154d9ce029c65e3ae64
kjb244 <kevin.bacino@gmail.com>
2018-02-06 19:49:41 -0500

    logging, cleaning up schedule page for mobile

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const pg = require('pg');
-const conStringLocal = "postgres://postgres:xxxx!@localhost:5432/wedding";
+const conStringLocal = "postgres://postgres:xxx@localhost:5432/wedding";
 const conString = process.env.DATABASE_URL || conStringLocal;
 let client = null;
 
@@ -10,6 +10,7 @@ class dbUtils{
 
     updateByEmail(email, payload){
         return new Promise(function(resolve, reject){
+            console.log(`updating db for email: ${email} with payload: ${payload}`)
             client = new pg.Client(conString);
             client.connect();
             let query = null;
@@ -34,8 +35,13 @@ class dbUtils{
                 where email = $3 and firstname = $4 and lastname = $5`;
                 queryArr = [payload.attending, currDate, email || '', payload.firstName || '', payload.lastName || ''];
             }
+            console.log(`before running update query: ${queryString} ${queryArr}`);
             client.query(queryString, queryArr, function(err, result){
-              resolve(result.rowCount);
+                if (err) {
+                   console.log(`update query error: ${error}`);
+                }
+                console.log(`update query ran successfully`);
+                resolve(result.rowCount);
             });
 
 
@@ -48,12 +54,14 @@ class dbUtils{
 
     getByEmail(email){
         return new Promise(function(resolve, reject){
+            console.log(`making email call from db`);
             client = new pg.Client(conString);
             client.connect();
 
             let query = client.query('select firstname, lastname, email, attending  from wedding_list where email =  $1 order by id, length(lastname)', [email]);
             let arr = [];
             query.on('error', function(err){
+                console.log(`email lookup db call failed for: ${email}`);
                 reject();
             });
             query.on('row', function(row){
@@ -67,6 +75,7 @@ class dbUtils{
 
             query.on('end', function(result){
                 client.end();
+                console.log(`email retrieved successfully from db`);
                 resolve(arr);
             })
         })
